f83cae348d39acf599387edd56ff034b2b3c09fb
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-04-19 10:19:38 +0200

    enabler: initial commit

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '.';
+
+const meta: Meta<typeof Button> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Button',
+  component: Button,
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const ClickMe: Story = {
+  args: {
+    label: 'click me',
+  },
+};
