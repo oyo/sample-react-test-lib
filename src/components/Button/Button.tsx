d42736b837a1f40d81feb99ac680ee3fdd8296a5
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-04-19 11:48:31 +0200

    enabler: added eslint

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,11 @@ import './Button.scss'
 
 export interface ButtonProps {
   label: string
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void
 }
 
-const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>
+const Button: React.FC<ButtonProps> = ({ label, onClick }: ButtonProps) => {
+  return <button onClick={onClick}>{label}</button>
 }
 
 export default Button
